bd28ce69f6b3651fa096fdaf92d26ae6782e0ad5
Eric Liu <eric.liu@defang.io>
2025-05-20 16:48:09 -0700

    update to have a default model
    make a default model

--- src/api/setting.py
+++ src/api/setting.py
@@ -16,7 +16,7 @@ FALLBACK_MODEL = os.environ.get("FALLBACK_MODEL")
 USE_MODEL_MAPPING = os.getenv("USE_MODEL_MAPPING", "true").lower() != "false"
 
 AWS_REGION = os.environ.get("AWS_REGION", "us-west-2")
-DEFAULT_MODEL = os.environ.get("DEFAULT_MODEL", "anthropic.claude-3-sonnet-20240229-v1:0")
+DEFAULT_MODEL = os.environ.get("DEFAULT_MODEL", "default")
 DEFAULT_EMBEDDING_MODEL = os.environ.get("DEFAULT_EMBEDDING_MODEL", "cohere.embed-multilingual-v3")
 ENABLE_CROSS_REGION_INFERENCE = os.environ.get("ENABLE_CROSS_REGION_INFERENCE", "true").lower() != "false"
 
