e62ee0cb82dabc882c9ea0553d504be36ad8a75d
herter4171-kp <herter@kairospower.com>
2025-09-23 16:31:00 -0400

    Print MD.

--- src/api/models/bedrock_agents.py
+++ src/api/models/bedrock_agents.py
@@ -303,6 +303,7 @@ class BedrockAgents(BedrockModel):
             
             if md.has_metadata:
                 md_args = md.get_metadata_args()
+                print(md_args)
                 query = md.get_clean_query()
                 kb_id = "D3Q2K57HXU"
 
